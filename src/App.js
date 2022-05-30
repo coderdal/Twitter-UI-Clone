f484951f17eaa0cc3390def627fc84d581939829
coderdal <agircoder@gmail.com>
2022-05-30 18:27:18 +0300

    General Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import styled from "styled-components";
+import LeftBar from "./components/LeftBar/LeftBar";
+import Main from "./components/Main/Main";
+import RightBar from "./components/RightBar/RightBar";
+
+const MainRoot = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
 const App = () => {
-  return <>@coderdal</>;
+  return (
+    <MainRoot>
+      <LeftBar />
+      <Main />
+      <RightBar />
+    </MainRoot>
+  );
 };
 
 export default App;
